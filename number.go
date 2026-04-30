f0b51fa7d6539f02f9c3625ea45179a80d140751
teshomenbret <teshomenbret4@gmail.com>
2026-04-30 17:15:22 +0300

    make it generic

--- number.go
+++ number.go
@@ -3,11 +3,19 @@
 
 package number
 
+import (
+	"golang.org/x/exp/constraints"
+)
+
+type Number interface {
+	constraints.Integer
+	constraints.Float
+}
 
 // Add add two intiger and return the sum
 //
 // It has two parameter: a  and b,the numbers to be added,
 // Let`s add this link to  test adding link [https://www.mathsisfun.com/numbers/addition.html]
-func Add(a, b int) int{
+func Add(a, b Number) Number{
 	return a + b
 }
