ce26906b3f2d0f8d62336812a8a42b059d39c0af
teshomenbret <teshomenbret4@gmail.com>
2026-04-30 17:09:24 +0300

    add docs

--- number.go
+++ number.go
@@ -1,5 +1,13 @@
+// Package provides  utlities to do 
+// operation on number
+
 package number
 
+
+// Add add two intiger and return the sum
+//
+// It has two parameter: a  and b,the numbers to be added,
+// Let`s add this link to  test adding link [https://www.mathsisfun.com/numbers/addition.html]
 func Add(a, b int) int{
 	return a + b
 }
